e81e2aa99d9870b6f6bd4ca7ec9eefc32e7ef8e6
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-15 06:46:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon (file, emoticon)
   emoticon_library = load_library (file)
   japanese_emoticon = emoticon_library["get_emoticon"][emoticon]
   if japanese_emoticon == nil
-    sorry_message
+    "Sorry emoticon not found"
   else
     japanese_emoticon
   end
