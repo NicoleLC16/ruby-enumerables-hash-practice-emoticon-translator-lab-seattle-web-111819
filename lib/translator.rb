3bab211525c940bdcc6bc986d2bf5e318a5ddeda
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-15 06:19:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,22 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library (file)
+load_emoticon = YAML.load_file(file) 
+
+emoticon_library = {}
+emoticon_library["get_meaning"] = Hash.new
+emoticon_library["get_emoticon"] = Hash.new
+
+load_emoticon.each do |word, emoticons|
+  emoticon_library["get_meaning"][emoticons[1]] = word 
+  emoticon_library["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  emoticon_library
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (file, emoticon)
+  load_library(file) = return_emoticon
+  return_emoticon["get_emoticon"][emoticon]
 end
 
 def get_english_meaning
