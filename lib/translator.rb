987e1a406a540f801d2b3a826564376ea125d238
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-15 06:28:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,14 +15,7 @@ load_emoticon.each do |word, emoticons|
 end
 
 def get_japanese_emoticon (file_path, emoticon)
-  library = load_library(file_path)
-  result = library["get_emoticon"][emoticon]
-  if result
-    result
-  else
-    "Sorry, that emoticon was not found"
-  end
-end
+
 end
 
 def get_english_meaning
